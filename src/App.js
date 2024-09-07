17749d749bed61978970fd2901d1d2573a18dc59
hisamc91237 <hisamc2000@gmail.com>
2024-09-07 14:34:22 +0530

    Header Section Completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/HeaderSection/Header";
 import { ThemeProvider } from "./useContextHook/useTheme";
+import Feed from "./components/FeedSection/Feed";
+import SearchVideoResult from "./components/SearchSection/SearchVideoResult";
+import VideoDetails from "./components/VideoSection/VideoDetails";
+import { AppContext } from "./useContextHook/useContextApi";
 
 function App() {
   return (
-    <ThemeProvider>
-      <BrowserRouter>
-        <div className="">
-          <Header />
-        </div>
-      </BrowserRouter>
-    </ThemeProvider>
+    <AppContext>
+      <ThemeProvider>
+        <BrowserRouter>
+          <div className="flex flex-col w-full">
+            <Header />
+            <Routes>
+              <Route path="/" element={<Feed />} />
+              <Route
+                path="/search/:searchQuery"
+                element={<SearchVideoResult />}
+              />
+              <Route
+                path="/video/:categoryId/:videoId"
+                element={<VideoDetails />}
+              />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </ThemeProvider>
+    </AppContext>
   );
 }
 
---
+++ src/components/VideoSection/RelatedVideos.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RelatedVideos = () => {
+  return <div></div>;
+};
+
+export default RelatedVideos;
